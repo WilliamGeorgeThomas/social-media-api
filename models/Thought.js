6574a7869be8f9a0aaca9894d3a2cf6855d67694
WGT <williamgeorgethomas@gmail.com>
2023-03-03 17:34:28 -0600

    created thoughtRoutes and thoughtController

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reaction");
+// const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
   {
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-    createdAt: [{ type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) }],
+    // createdAt: [{ type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp) }],
     username: [{ type: String, required: true }],
-    reactions: [reactionSchema],
+    // reactions: [reactionSchema],
   },
   {
     toJSON: {
