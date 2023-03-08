0a7c13e2810170ce53656bb5500fe9012897cef3
WGT <williamgeorgethomas@gmail.com>
2023-03-07 18:32:12 -0600

    created addFriend and removeFriend in routes and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,5 +30,17 @@ module.exports = {
       .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json({ message: "user deleted" })))
       .catch((err) => res.status(500).json(err));
   },
-//add friend, remove friend
+  //add friend, remove friend
+
+  addFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user found with that ID :(" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove assignment from a student
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: { userId: req.params.friendId } } }, { runValidators: true, new: true })
+      .then((user) => (!user ? res.status(404).json({ message: "No user found with that ID :(" }) : res.json(user)))
+      .catch((err) => res.status(500).json(err));
+  },
 };
